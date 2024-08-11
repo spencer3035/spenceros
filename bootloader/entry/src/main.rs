f1c2e1a121df55b734baad05b98145d146fab9d2
Spencer <spencer3035@gmail.com>
2024-08-10 20:58:52 -0600

    Trying to read disk is giving me an error

--- bootloader/entry/src/main.rs
+++ bootloader/entry/src/main.rs
@@ -1,70 +1,80 @@
 #![no_std]
 #![no_main]
 
+global_asm!(include_str!("boot.s"));
+
 use core::arch::asm;
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-global_asm!(include_str!("boot.s"));
+pub mod debug;
+use debug::*;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     fail(b'P');
 }
 
-fn print_char(c: &u8) {
-    let ax = *c as u16 | 0x0e00;
-    unsafe {
-        asm!(
-            "int 0x10",
-            in("ax") ax,
-        );
-    }
-}
-
-fn print_dec(mut num: u16) {
-    let mut num_digits = 0;
-
-    loop {
-        let digit: u16 = (num % 10).into();
-        unsafe {
-            asm!("push {0:x}", in(reg) digit);
-        }
-        num_digits += 1;
-        num /= 10;
-        if num == 0 {
-            break;
-        }
-    }
-
-    while num_digits >= 1 {
-        let digit: i16;
-        unsafe {
-            asm!("pop {0:x}", out(reg) digit);
-        }
-        let value = digit as u8 + b'0';
-        print_char(&value);
-        num_digits -= 1;
-    }
-}
-
-fn println(chars: &[u8]) {
-    print(chars);
-    print(b"\r\n");
-}
-
-fn print(chars: &[u8]) {
-    for val in chars.iter() {
-        print_char(val);
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn main(drive_number: u8) {
-    println(b"Main.");
-    print_dec(123);
-    println(b"\r\n");
+    //println(b"Reading");
+    load_sector(drive_number);
+    let char_out_of_range: u16;
+    unsafe {
+        asm!(
+            "mov ax, [0x7e01]",
+            "mov {0:x}, ax",
+            out(reg) char_out_of_range
+        );
+    }
+
+    print_char(&(char_out_of_range as u8));
+    //println(b"Done");
+    //print_dec(123);
+    //println(b"\r\n");
     hlt();
+}
+
+fn load_sector(drive_number: u8) {
+    let mut num_sectors: u8 = 1;
+    let requested_sectors = num_sectors;
+    let mut to_address: u16 = 0x7e00;
+    let exit_status: u8;
+
+    // TODO: Something is broken here
+    unsafe {
+        asm!(
+            "mov bx, {0:x}", // Address to load into
+            "mov ah, 2", // 2 for reading disk to memory
+            "mov al, {2}", // Number of sectors to read
+            "mov ch, 0", // Cylander number
+            "mov cl, 1", // Sector number
+            "mov dh, 0", // Head number
+            "mov dl, {1}", // Drive number
+            "int 0x13", // Perform read inturrupt
+            "mov dx, 0", // Put Carry flag into ax
+            "adc dx, 0",
+            "mov {0:x}, dx",
+            "mov {3}, ah",
+            inout(reg) to_address,
+            in(reg_byte) drive_number,
+            inout(reg_byte) num_sectors,
+            out(reg_byte) exit_status,
+        )
+    }
+    // TODO: This is wrong, change it to ==
+    //let disk_read_ok = to_address == 0;
+    let disk_read_ok = exit_status == 0;
+
+    if !disk_read_ok {
+        print_dec(exit_status.into());
+        fail(b'd');
+    }
+
+    let read_sectors = num_sectors;
+    if requested_sectors != read_sectors {
+        fail(b'r');
+    }
 }
 
 /// Prints '![char]' where [char] should be the top element on the stack when this is called
@@ -77,9 +87,7 @@ pub extern "C" fn fail(code: u8) -> ! {
     print(b"Fail: ");
     print_char(&code);
     print(b"\r\n");
-    loop {
-        hlt()
-    }
+    hlt()
 }
 
 /// Halts CPU
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ const BOOT_BYTES: &[u8] = include_bytes!(env!("BIOS_ENTRY"));
 const EXTRA_BYTES: [u8; 512] = [b'A'; 512];
 
 fn main() {
-    println!("got bios of length === {}", BOOT_BYTES.len());
-    let disk_bytes: Vec<u8> = BOOT_BYTES
+    let mut disk_bytes: Vec<u8> = BOOT_BYTES
         .iter()
         .chain(EXTRA_BYTES.iter())
         .cloned()
         .collect();
+    disk_bytes.push(b'z');
 
     let disk_image_file = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("target")
