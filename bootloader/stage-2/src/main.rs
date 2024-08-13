4601b385a9ce7e722f6c07fe409e302ea905aa01
Spencer <spencer3035@gmail.com>
2024-08-12 23:57:18 -0600

    Got debug function for printing memory kind of working

--- bootloader/stage-2/src/main.rs
+++ bootloader/stage-2/src/main.rs
@@ -18,11 +18,52 @@ pub extern "C" fn _start(_disk_number: u16) -> ! {
         hlt();
     }
 
+    println!("MEM:");
+
+    // TODO: Set sp/bp and verify that the memory is set as expected. We could also just print the
+    // sp/bp values...
+    // This doesn't work
+    //unsafe {
+    //    asm!(
+    //    "mov eax, {stack_start}",
+    //    "mov esp, eax",
+    //    "mov ebp, 0",
+    //    "push 7",
+    //    stack_start = in(reg) STACK_END,
+    //    )
+    //}
+    let start = STACK_END;
+    let mut val = 0;
+    //for ii in 0..30 {
+    //    unsafe { start.add(ii).write(0xf) }
+    //    val += 1;
+    //}
+    unsafe {
+        //start.add(0).write(0x12);
+        //start.add(1).write(0x34);
+        //start.add(2).write(0xff);
+        print_memory_addresses(STACK_END.sub(16));
+    }
+
     // TODO:
     // Set up paging
     // Load/update gdt to have long mode
     // Enter Long Mode
     hlt();
+}
+
+unsafe fn print_memory_addresses(mut start: *const u8) {
+    let size = 16;
+    //let num_lines = 20;
+    //for ii in 0..num_lines {
+    //    let as_arr = core::slice::from_raw_parts(start, size);
+    //    print!("0x{:04x}: ", start as u64);
+    //    println!("{as_arr:02x?}");
+    //    start = start.add(16);
+    //}
+    let as_arr = core::slice::from_raw_parts(start, size);
+    print!("0x{:04x}: ", start as u64);
+    println!("{as_arr:02x?}");
 }
 
 fn setup_paging() {
