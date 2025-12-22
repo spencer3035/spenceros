22b231dd7b88bb0297b0daf87f31e00283542b8d
Spencer <spencer3035@gmail.com>
2025-12-22 10:29:10 -0700

    Cleaned up print statements

--- bootloader/stage-1/src/main.rs
+++ bootloader/stage-1/src/main.rs
@@ -35,7 +35,7 @@ use crate::mem::detect_memory;
 fn init_static_values() {
     // SAFETY: These should only be called once, we call them here
     unsafe {
-        // BiosInfo::init();
+        BiosInfo::init();
         Font::init();
         VbeDisplayInfo::init();
         MemInfo::init();
@@ -54,40 +54,19 @@ fn main(disk_number: u16) {
         panic!("CPUID not present");
     }
 
-    // prompt_continue();
     init_graphical();
-    println_vbe!("update bios is at 0x{:X}", update_bios_info as usize);
-    println_vbe!("_start is at 0x{:X}", _start as usize);
     update_bios_info(disk_number);
 
     // Safety: This is the only mutable reference.
-    // let memory_info = unsafe { MemInfo::get_mut() };
-    // detect_memory(memory_info);
+    let memory_info = unsafe { MemInfo::get_mut() };
+    detect_memory(memory_info);
 }
 
 #[inline(never)]
 fn update_bios_info(disk_number: u16) {
-    // println_vbe!("trying to write = {}", disk_number);
     unsafe {
-        {
-            // let info: &BiosInfo = &*BIOS_INFO;
-            // println_vbe!("addr = 0x{:X}", BIOS_INFO as usize);
-            // println_vbe!("before = {}", info.disk_number);
-        }
-        {
-            let info: &mut BiosInfo = &mut *BIOS_INFO;
-            info.disk_number = disk_number;
-
-            // let info = BiosInfo {
-            //     // disk_number: 128,
-            //     disk_number,
-            // };
-            // BIOS_INFO.write(info);
-        }
-        {
-            // let info: &BiosInfo = &*BIOS_INFO;
-            // println_vbe!("after = {}", info.disk_number);
-        }
+        let info: &mut BiosInfo = &mut *BIOS_INFO;
+        info.disk_number = disk_number;
     }
 }
 
@@ -96,7 +75,6 @@ fn update_bios_info(disk_number: u16) {
 pub extern "C" fn _start(disk_number: u16) {
     // utils::print_unsafe_fn_location(next_stage);
     main(disk_number);
-    println_vbe!("disk_number : {disk_number}");
     unsafe {
         next_stage();
     }
